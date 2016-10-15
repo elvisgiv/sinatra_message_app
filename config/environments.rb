158251aedb47ed5751e0081a02f07a4ffce97418
elvis <alex.danko@galacticexchange.io>
2016-10-15 20:26:09 +0300

    fix for herok

--- config/environments.rb
+++ config/environments.rb
@@ -5,7 +5,7 @@ configure :production, :development, :test do
   ActiveRecord::Base.establish_connection(
       :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
       :host     => db.host,
-      :username => 'postgres',#db.user,
+      :username => db.user,#'postgres'
       :password => db.password,
       :database => db.path[1..-1],
       :encoding => 'utf8'
